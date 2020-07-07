7e0874e66f9865a16f9bb2a241acfdfb447a265d
macbookpro <abenov2005.uzer@gmail.com>
2020-07-07 06:05:40 +0600

    HomeWork 3-4: did link

--- app/src/main/java/com/example/todo/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/todo/ui/home/HomeFragment.java
@@ -1,5 +1,9 @@
 package com.example.todo.ui.home;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 
@@ -41,6 +45,7 @@ public class HomeFragment extends Fragment {
     private LottieAnimationView lottieAnimationLike;
     private TextView textExplore;
     private TextView textPrice;
+    private TextView textLink;
     private View viewPersonCircle1;
     private View viewPersonCircle2;
     private View viewPersonCircle3;
@@ -91,6 +96,7 @@ public class HomeFragment extends Fragment {
         lottieAnimationLike = view.findViewById(R.id.lottieAnimation_main_like);
         textExplore = view.findViewById(R.id.text_main_explore);
         textPrice = view.findViewById(R.id.text_main_free);
+        textLink = view.findViewById(R.id.text_main_link);
         viewPersonCircle1 = view.findViewById(R.id.view_main_person_circle_1);
         viewPersonCircle2 = view.findViewById(R.id.view_main_person_circle_2);
         viewPersonCircle3 = view.findViewById(R.id.view_main_person_circle_3);
@@ -117,9 +123,6 @@ public class HomeFragment extends Fragment {
                 if (spinnerSelectedValues != null) {
                     if (spinnerSelectedValues.equals("RANDOM")) {
                         spinnerSelectedValues = null;
-                    } else if (spinnerSelectedValues.equals("Category")) {
-                        Toast.makeText(getContext(), "Выберите категорию", Toast.LENGTH_SHORT).show();
-                        return;
                     }
                 }
                 App.boredAPIClient.getAction(
@@ -134,24 +137,8 @@ public class HomeFragment extends Fragment {
                                 textCategory.setText(boredAction.getType());
                                 textExplore.setText(boredAction.getActivity());
                                 textPrice.setText(boredAction.getPrice().toString() + "$");
-                                switch (boredAction.getParticipants()) {
-                                    case 1:
-                                        recoveryParticipantsViews();
-                                        invisibleParticipantsCase1();
-                                        break;
-                                    case 2:
-                                        recoveryParticipantsViews();
-                                        invisibleParticipantsCase2();
-                                        break;
-                                    case 3:
-                                        recoveryParticipantsViews();
-                                        invisibleParticipantsCase3();
-                                        break;
-                                    case 4:
-                                        recoveryParticipantsViews();
-                                        invisibleParticipantsCase4();
-                                        break;
-                                }
+                                createParticipants(boredAction);
+                                createLink(boredAction);
                                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
                                     progressBarAccessibility.setProgress((int) (boredAction.getAccessibility() * 100), true);
                                 }
@@ -166,6 +153,27 @@ public class HomeFragment extends Fragment {
             }
         });
 
+    }
+
+    private void createParticipants(BoredAction boredAction) {
+        switch (boredAction.getParticipants()) {
+            case 1:
+                recoveryParticipantsViews();
+                invisibleParticipantsCase1();
+                break;
+            case 2:
+                recoveryParticipantsViews();
+                invisibleParticipantsCase2();
+                break;
+            case 3:
+                recoveryParticipantsViews();
+                invisibleParticipantsCase3();
+                break;
+            case 4:
+                recoveryParticipantsViews();
+                invisibleParticipantsCase4();
+                break;
+        }
     }
 
     private void recoveryParticipantsViews() {
@@ -197,6 +205,21 @@ public class HomeFragment extends Fragment {
         viewPersonCircle1.setVisibility(View.INVISIBLE);
         viewPersonCircle2.setVisibility(View.INVISIBLE);
         viewPersonCircle3.setVisibility(View.INVISIBLE);
+    }
+
+    private void createLink(BoredAction boredAction) {
+        if (boredAction.getLink().equals("")) {
+            textLink.setTypeface(Typeface.DEFAULT_BOLD);
+            textLink.setText(boredAction.getLink());
+            textLink.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String URL = boredAction.getLink();
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(URL));
+                    startActivity(intent);
+                }
+            });
+        }
     }
 
     private boolean flag = true;
