6eae58b262942e3285fd196f98e7245e2cc232de
macbookpro <abenov2005.uzer@gmail.com>
2020-07-13 18:59:29 +0600

    HomeWork 5: correction

--- app/src/main/java/com/example/todo/UI/settings/SettingsFragment.java
+++ app/src/main/java/com/example/todo/UI/settings/SettingsFragment.java
@@ -7,12 +7,10 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.Toast;
 
 import com.example.todo.App;
 import com.example.todo.R;
