688dc38f75e524d3fd9568238681c1ac69f02f2c
macbookpro <abenov2005.uzer@gmail.com>
2020-07-16 08:22:15 +0600

    HomeWork 6: corrections

--- app/src/main/java/com/example/bored/presentation/UI/onboarding/IntroActivity.java
+++ app/src/main/java/com/example/bored/presentation/UI/onboarding/IntroActivity.java
@@ -3,12 +3,14 @@ package com.example.bored.presentation.UI.onboarding;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.bored.App;
 import com.example.bored.R;
+import com.example.bored.presentation.UI.main.MainActivity;
 import com.tbuonomo.viewpagerdotsindicator.WormDotsIndicator;
 
 public class IntroActivity extends AppCompatActivity {
@@ -39,6 +41,7 @@ public class IntroActivity extends AppCompatActivity {
 
     public void skipClick(View view) {
         App.appPreferences.setLaunched();
+        startActivity(new Intent(this, MainActivity.class));
         finish();
     }
 
